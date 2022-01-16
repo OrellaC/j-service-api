b9509aeaded092068ca04c5daca94fed6058421b
orellachichester@gmail.com <orellachichester@gmail.com>
2022-01-15 20:27:01 -0500

    Submission Commit

--- jeopardy-app/src/App.js
+++ jeopardy-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../src/CSS/App.css'
+import Score from './Score';
 import GameInfo from './GameInfo';
 
 class App extends Component {
@@ -8,8 +9,8 @@ class App extends Component {
     jeopardyInfo: {},
     show: false
   }
-  //2.  On click, have this button that will make a GET request (using fetch) to get random trivia data. Let's start you off with a win by giving you the URL: http://jservice.io/api/random
 
+  //2.  On click, have this button that will make a GET request (using fetch) to get random trivia data. Let's start you off with a win by giving you the URL: http://jservice.io/api/random
 
   handleSubmit = e => {
     e.preventDefault()
@@ -38,21 +39,13 @@ class App extends Component {
       // 3. Make some html elements like a div where you will display the question, category and points. NOTE: You will need to look at the data that is returned to you and think back about how to access data from different datatypes (is it an array within an object? How do you access the info?).
 
       <div id="container">
+
         <div className='header'>
           <h1>Welcome to Jeopardy!</h1>
         </div>
-        {/*7. Make a div that has an h2 and two buttons */}
-        <div className='button-box'>
+          
+          <Score />
 
-          {/* 8. Inside the h2 keep score, start at 0  */}
-          <h2> Score: 0</h2>
-
-          {/* 9. Make one button add the points to the score and the other to subtract points from the score (just add or subtract 1 point - using the points value from jService is Hungry for More) */}
-          <button type="submit" className='add'>Add Points </button>
-
-          <button type="submit" className='subtract'>Subtract Points </button>
-
-        </div>
 
         {/* 1. Make a button labeled "Random Trivia Question" or "Get Question" or something descriptive. */}
         <h2>Let's Play!</h2>
@@ -68,8 +61,6 @@ class App extends Component {
         </div>
 
         <h4 style={{ display: (show ? "block" : "none") }}>  {this.state.jeopardyInfo?.answer} </h4>
-
-
 
 
       </div>
--- jeopardy-app/src/Score.js
+++ jeopardy-app/src/Score.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
+import '../src/CSS/App.css'
+
+// I used this resource to create this component https://medium.com/@aghh1504/2-increment-and-decrease-number-onclick-react-5767b765103c
 
 class Score extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            clicks: 0,
+            show: true
+        };
+    }
+
+    IncrementItem = () => {
+        this.setState({ clicks: this.state.clicks + 100 });
+    }
+    DecreaseItem = () => {
+        this.setState({ clicks: this.state.clicks - 100 });
+    }
+
+
     render() {
         return (
+            /*7. Make a div that has an h2 and two buttons */
             <div>
-                
+                {/* 9. Make one button add the points to the score and the other to subtract points from the score (just add or subtract 1 point - using the points value from jService is Hungry for More) */}
+
+                <button className='add' onClick={this.IncrementItem}>Add Points </button>
+                <button className='subtract' onClick={this.DecreaseItem}>Subtract Points</button>
+
+                {/* 8. Inside the h2 keep score, start at 0  */}
+
+                <h2> Score: {this.state.clicks} </h2>
             </div>
         );
     }
 }
-
 export default Score;
+
 
