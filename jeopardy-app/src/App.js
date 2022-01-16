ddde26af2d1b1635c56c64dd2f00834b74510a16
orellachichester@gmail.com <orellachichester@gmail.com>
2022-01-15 20:02:11 -0500

    Sixth Commit

--- jeopardy-app/src/App.js
+++ jeopardy-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css'
+import '../src/CSS/App.css'
 import GameInfo from './GameInfo';
 
 class App extends Component {
@@ -36,28 +36,42 @@ class App extends Component {
     return (
 
       // 3. Make some html elements like a div where you will display the question, category and points. NOTE: You will need to look at the data that is returned to you and think back about how to access data from different datatypes (is it an array within an object? How do you access the info?).
-     
-     <div id="container">
 
-        <div>
-          <h2>Welcome to Jeopardy!</h2>
-          {/* 1. Make a button labeled "Random Trivia Question" or "Get Question" or something descriptive. */}
-          <button type="submit" onClick={this.handleSubmit}>Get Question</button>
+      <div id="container">
+        <div className='header'>
+          <h1>Welcome to Jeopardy!</h1>
+        </div>
+        {/*7. Make a div that has an h2 and two buttons */}
+        <div className='button-box'>
 
-          <GameInfo jeopardyInfo={this.state.jeopardyInfo} />
+          {/* 8. Inside the h2 keep score, start at 0  */}
+          <h2> Score: 0</h2>
 
-          {/* 4. Make another div in your HTML where you display the answer */}
-          {/* 5. Add an on click, that has a function that will toggle whether or not you can see the answer. */}
+          {/* 9. Make one button add the points to the score and the other to subtract points from the score (just add or subtract 1 point - using the points value from jService is Hungry for More) */}
+          <button type="submit" className='add'>Add Points </button>
 
-<div className='answer-box'>
-          <button type="submit" onClick={() => this.setState({ show: !show })}> Answer </button>
-        
-
-          <h4 style={{ display: (show ? "block" : "none") }}> Answer: {this.state.jeopardyInfo?.answer} </h4>
-          </div>
-
+          <button type="submit" className='subtract'>Subtract Points </button>
 
         </div>
+
+        {/* 1. Make a button labeled "Random Trivia Question" or "Get Question" or something descriptive. */}
+        <h2>Let's Play!</h2>
+        <button type="submit" className='question' onClick={this.handleSubmit}>Get Question</button>
+
+        <GameInfo jeopardyInfo={this.state.jeopardyInfo} />
+
+        {/* 4. Make another div in your HTML where you display the answer */}
+        {/* 5. Add an on click, that has a function that will toggle whether or not you can see the answer. */}
+
+        <div className='answer-box'>
+          <button type="submit" className='answer' onClick={() => this.setState({ show: !show })}> Answer </button>
+        </div>
+
+        <h4 style={{ display: (show ? "block" : "none") }}>  {this.state.jeopardyInfo?.answer} </h4>
+
+
+
+
       </div>
     );
   }
--- jeopardy-app/src/GameInfo.js
+++ jeopardy-app/src/GameInfo.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
+import '../src/CSS/App.css'
 
 class GameInfo extends Component {
     render(props) {
         return (
-            <div>
-                <h2>Score: 0 </h2>
-                <h1>Let's Play</h1>
+            <div className='game-box'>
                 <h2>Category: {this.props.jeopardyInfo.category?.title}</h2>
                 <h2>Points: {this.props.jeopardyInfo?.value}</h2>
                 <h2>Question: {this.props.jeopardyInfo?.question}</h2>
